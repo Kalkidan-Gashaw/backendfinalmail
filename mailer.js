bd68292ca2b7bed0fb6c59669448e51b29822c1f
Kalkidan-Gashaw <162141690+Kalkidan-Gashaw@users.noreply.github.com>
2025-01-30 10:59:36 +0300

    Update mailer.js

--- mailer.js
+++ mailer.js
@@ -9,7 +9,7 @@ const transporter = nodemailer.createTransport({
 });
 
 export const sendVerificationEmail = (email, token) => {
-  const verificationLink = `http://localhost:5173/users/verify/?token=${token}`;
+  const verificationLink = `https://fmailfinal.vercel.app/users/verify/?token=${token}`;
 
   const mailOptions = {
     from: "gashawkalkidan700@gmail.com",
